8b22eef2fd7fc59bfa46884172dfa6d45ccddf1b
Андрей <@jay_gatz>
2024-08-28 19:27:39 +0700

    RA-04 refactor fragment initialization: replace() function replaced by add()

--- app/src/main/java/com/example/recipesapp/MainActivity.kt
+++ app/src/main/java/com/example/recipesapp/MainActivity.kt
@@ -4,8 +4,8 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.add
 import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import com.example.recipesapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -28,7 +28,8 @@ class MainActivity : AppCompatActivity() {
 
         if (savedInstanceState == null) {
             supportFragmentManager.commit {
-                replace<CategoriesListFragment>(R.id.mainContainer)
+                setReorderingAllowed(true)
+                add<CategoriesListFragment>(R.id.mainContainer)
             }
         }
     }
