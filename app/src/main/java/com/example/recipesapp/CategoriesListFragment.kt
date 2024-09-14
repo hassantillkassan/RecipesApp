3ef19a63438285af8e45b29cb7416ee602245760
Андрей <@jay_gatz>
2024-09-15 00:10:45 +0700

    RA-08 refactor: margonBottom for category recipe card

--- app/src/main/java/com/example/recipesapp/CategoriesListFragment.kt
+++ app/src/main/java/com/example/recipesapp/CategoriesListFragment.kt
@@ -32,9 +32,6 @@ class CategoriesListFragment : Fragment() {
 
         val adapter = CategoriesListAdapter(categories)
         categoriesBinding.rvCategories.adapter = adapter
-
-        val itemDecoration = SpaceItemDecoration(resources.getDimensionPixelSize(R.dimen.item_spacing))
-        categoriesBinding.rvCategories.addItemDecoration(itemDecoration)
     }
 
     override fun onDestroyView() {
