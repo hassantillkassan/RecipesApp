8224803f83cfd13b43e5dc1550b483b9c259f970
Андрей <@jay_gatz>
2024-12-14 21:39:04 +0700

    RA-37 Coroutine implementation

--- app/src/main/java/com/example/recipesapp/data/RecipeApiService.kt
+++ app/src/main/java/com/example/recipesapp/data/RecipeApiService.kt
@@ -2,7 +2,6 @@ package com.example.recipesapp.data
 
 import com.example.recipesapp.model.Category
 import com.example.recipesapp.model.Recipe
-import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -10,18 +9,18 @@ import retrofit2.http.Query
 interface RecipeApiService {
 
     @GET("category")
-    fun getCategories(): Call<List<Category>>
+    suspend fun getCategories(): List<Category>
 
     @GET("category/{id}/recipes")
-    fun getRecipesByCategoryId(@Path("id") categoryId: Int): Call<List<Recipe>>
+    suspend fun getRecipesByCategoryId(@Path("id") categoryId: Int): List<Recipe>
 
     @GET("category/{id}")
-    fun getCategoryById(@Path("id") categoryId: Int): Call<Category>
+    suspend fun getCategoryById(@Path("id") categoryId: Int): Category
 
     @GET("recipes")
-    fun getRecipesByIds(@Query("ids") ids: String): Call<List<Recipe>>
+    suspend fun getRecipesByIds(@Query("ids") ids: String): List<Recipe>
 
     @GET("recipe/{id}")
-    fun getRecipeById(@Path("id") recipeId: Int): Call<Recipe>
+    suspend fun getRecipeById(@Path("id") recipeId: Int): Recipe
 
 }
