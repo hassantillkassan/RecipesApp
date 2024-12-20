68fffe8de320a6bdeb41850fbb304b0795818110
Андрей <@jay_gatz>
2024-12-20 23:18:18 +0700

    RA-40 Caching implementation for Recipes list

--- app/src/main/java/com/example/recipesapp/ui/recipes/list/RecipesListViewModel.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipes/list/RecipesListViewModel.kt
@@ -33,41 +33,84 @@ class RecipesListViewModel(application: Application) : AndroidViewModel(applicat
 
     fun loadRecipe(category: Category) {
         viewModelScope.launch {
-            try {
-                val recipes = recipesRepository.getRecipesByCategoryId(category.id)
+            _state.value = _state.value?.copy(error = null)
 
-                if (recipes != null) {
-                    val updatedRecipes = recipes.map { recipe ->
-                        recipe.copy(imageUrl = Constants.BASE_URL + Constants.IMAGES_PATH + recipe.imageUrl)
+            val cachedRecipes = recipesRepository.getAllCachedRecipes()
+            val filteredRecipes = cachedRecipes.filter { it.categoryId == category.id }
+
+            if (filteredRecipes.isEmpty()) {
+                val networkRecipes = recipesRepository.getRecipesByCategoryId(category.id)
+
+                if (networkRecipes != null) {
+                    val networkRecipesWithCategory = networkRecipes.map { recipe ->
+                        recipe.copy(
+                            categoryId = category.id,
+                            imageUrl = Constants.BASE_URL + Constants.IMAGES_PATH + recipe.imageUrl,
+                        )
                     }
 
-                    _state.postValue(
-                        _state.value?.copy(
-                            recipes = updatedRecipes,
-                            categoryName = category.title,
-                            categoryImage = category.imageUrl,
-                            error = null,
-                        )
+                    recipesRepository.saveRecipesToCache(networkRecipesWithCategory)
+
+                    _state.value = _state.value?.copy(
+                        recipes = networkRecipesWithCategory,
+                        categoryName = category.title,
+                        categoryImage = category.imageUrl,
+                        error = null,
                     )
                 } else {
-                    _state.postValue(
-                        _state.value?.copy(
-                            error = ErrorType.DATA_FETCH_ERROR,
-                        )
+                    _state.value = _state.value?.copy(
+                        error = ErrorType.DATA_FETCH_ERROR,
                     )
                 }
-            } catch (e: Exception) {
-                Log.e("RecipesListViewModel", "Ошибка при загрузке списка рецептов", e)
-                _state.postValue(
-                    _state.value?.copy(
-                        error = ErrorType.UNKNOWN_ERROR,
-                    )
+            } else {
+               _state.value = _state.value?.copy(
+                    recipes = filteredRecipes,
+                    categoryName = category.title,
+                    categoryImage = category.imageUrl,
+                    error = null,
                 )
             }
+
+            /*_state.value = _state.value?.copy(error = null)
+
+            val cachedRecipes = recipesRepository.getAllCachedRecipes()
+            val filteredRecipes = cachedRecipes.filter { it.categoryId == category.id }
+
+            if (filteredRecipes.isEmpty()) {
+                val networkRecipes = recipesRepository.getRecipesByCategoryId(category.id)
+
+                if (networkRecipes != null) {
+                    recipesRepository.saveRecipesToCache(networkRecipes)
+
+                    _state.value = _state.value?.copy(
+                        recipes = networkRecipes.map { recipe ->
+                            recipe.copy(imageUrl = Constants.BASE_URL + Constants.IMAGES_PATH + recipe.imageUrl)
+                        },
+                        categoryName = category.title,
+                        categoryImage = category.imageUrl,
+                        error = null,
+                    )
+                } else {
+                    _state.value = _state.value?.copy(
+                        error = ErrorType.DATA_FETCH_ERROR,
+                    )
+                }
+            } else {
+                val updatedCachedRecipes = filteredRecipes.map { recipe ->
+                    recipe.copy(imageUrl = Constants.BASE_URL + Constants.IMAGES_PATH + recipe.imageUrl)
+                }
+
+                _state.value = _state.value?.copy(
+                    recipes = updatedCachedRecipes,
+                    categoryName = category.title,
+                    categoryImage = category.imageUrl,
+                    error = null,
+                )
+            }*/
         }
     }
 
     fun clearError() {
-        _state.postValue(_state.value?.copy(error = null))
+        _state.value = _state.value?.copy(error = null)
     }
 }
