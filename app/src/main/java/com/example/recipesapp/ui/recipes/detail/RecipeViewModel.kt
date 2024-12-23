c4bc7a8019d33f7e73abf378c28dd4c06ffc25d9
Андрей <@jay_gatz>
2024-12-23 23:58:50 +0700

    RA-41 refactor: removed SharedPreferences

--- app/src/main/java/com/example/recipesapp/ui/recipes/detail/RecipeViewModel.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipes/detail/RecipeViewModel.kt
@@ -1,13 +1,11 @@
 package com.example.recipesapp.ui.recipes.detail
 
 import android.app.Application
-import android.content.Context
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.recipesapp.common.Constants
 import com.example.recipesapp.data.AppDatabase
 import com.example.recipesapp.data.RecipesRepository
 import com.example.recipesapp.model.ErrorType
@@ -37,17 +35,12 @@ class RecipeViewModel(application: Application) : AndroidViewModel(application) 
             try {
                 val recipe = recipesRepository.getRecipeById(recipeId)
 
-                val favorites = getFavorites().toMutableSet()
-                val isFavorite = favorites.contains(recipeId.toString())
-
-                val recipeImageUrl = Constants.BASE_URL + Constants.IMAGES_PATH + recipe?.imageUrl
-
                 if (recipe != null) {
                     _state.postValue(
                         _state.value?.copy(
-                            recipe = recipe.copy(isFavorite = isFavorite),
-                            isFavorite = isFavorite,
-                            recipeImage = recipeImageUrl,
+                            recipe = recipe.copy(isFavorite = recipe.isFavorite),
+                            isFavorite = recipe.isFavorite,
+                            recipeImage = recipe.updatedImageUrl,
                             error = null,
                         )
                     )
@@ -66,83 +59,18 @@ class RecipeViewModel(application: Application) : AndroidViewModel(application) 
                     )
                 )
             }
-
-            /*try {
-                val recipe = recipesRepository.getRecipeById(recipeId)
-
-                val favorites = getFavorites()
-
-                val recipeImageUrl = Constants.BASE_URL + Constants.IMAGES_PATH + recipe?.imageUrl
-
-                if (recipe != null) {
-                    val updatedRecipe =
-                        recipe.copy(imageUrl = Constants.BASE_URL + Constants.IMAGES_PATH + recipe.imageUrl)
-                    val isFavorite = favorites.contains(recipeId.toString())
-
-                    _state.postValue(
-                        _state.value?.copy(
-                            recipe = updatedRecipe,
-                            isFavorite = isFavorite,
-                            recipeImage = recipeImageUrl,
-                            error = null,
-                        )
-                    )
-                } else {
-                    _state.postValue(
-                        _state.value?.copy(
-                            error = ErrorType.DATA_FETCH_ERROR,
-                        )
-                    )
-                }
-            } catch (e: Exception) {
-                Log.e("RecipeViewModel", "Ошибка при загрузке рецепта", e)
-                _state.postValue(
-                    _state.value?.copy(
-                        error = ErrorType.UNKNOWN_ERROR,
-                    )
-                )
-            }*/
         }
-    }
-
-    private fun getFavorites(): MutableSet<String> {
-        val sharedPrefs =
-            getApplication<Application>().getSharedPreferences(
-                Constants.SHARED_PREFS_NAME,
-                Context.MODE_PRIVATE
-            )
-        val favorites =
-            sharedPrefs.getStringSet(Constants.FAVORITES_KEY, emptySet()) ?: emptySet()
-
-        return HashSet(favorites)
-    }
-
-    private fun saveFavorites(favorites: Set<String>) {
-        val sharedPrefs =
-            getApplication<Application>().getSharedPreferences(
-                Constants.SHARED_PREFS_NAME,
-                Context.MODE_PRIVATE
-            )
-        sharedPrefs.edit().putStringSet(Constants.FAVORITES_KEY, favorites).apply()
     }
 
     fun onFavoritesClicked() {
         val recipeId = _state.value?.recipe?.id ?: return
-        val favorites = getFavorites().toMutableSet()
         val isCurrentlyFavorite = state.value?.isFavorite ?: false
-
-        if (isCurrentlyFavorite) {
-            favorites.remove(recipeId.toString())
-        } else {
-            favorites.add(recipeId.toString())
-        }
-
-        saveFavorites(favorites)
-        _state.value = _state.value?.copy(isFavorite = !isCurrentlyFavorite)
 
         viewModelScope.launch {
             recipesRepository.updateRecipeFavoriteStatus(recipeId, !isCurrentlyFavorite)
         }
+
+        _state.value = _state.value?.copy(isFavorite = !isCurrentlyFavorite)
     }
 
     fun updatePortionCount(portionCount: Int) {
--- app/src/main/java/com/example/recipesapp/ui/recipes/favorites/FavoritesViewModel.kt
+++ app/src/main/java/com/example/recipesapp/ui/recipes/favorites/FavoritesViewModel.kt
@@ -1,13 +1,11 @@
 package com.example.recipesapp.ui.recipes.favorites
 
 import android.app.Application
-import android.content.Context
 import android.util.Log
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
-import com.example.recipesapp.common.Constants
 import com.example.recipesapp.data.AppDatabase
 import com.example.recipesapp.data.RecipesRepository
 import com.example.recipesapp.model.ErrorType
@@ -62,70 +60,7 @@ class FavoritesViewModel(application: Application) : AndroidViewModel(applicatio
                     )
                 )
             }
-
-            /*try {
-                val favoritesIds = getFavorites()
-
-                if (favoritesIds.isEmpty()) {
-                    _state.postValue(
-                        _state.value?.copy(
-                            recipes = emptyList(),
-                            isEmpty = true,
-                            isLoading = false,
-                            error = null,
-                        )
-                    )
-
-                    return@launch
-                }
-
-                val favoriteRecipes = recipesRepository.getRecipesByIds(favoritesIds)
-
-                if (favoriteRecipes != null) {
-                    val updatedFavoriteRecipe = favoriteRecipes.map { recipe ->
-                        recipe.copy(imageUrl = Constants.BASE_URL + Constants.IMAGES_PATH + recipe.imageUrl)
-                    }
-
-                    _state.postValue(
-                        _state.value?.copy(
-                            recipes = updatedFavoriteRecipe,
-                            isEmpty = favoriteRecipes.isEmpty(),
-                            isLoading = false,
-                            error = null,
-                        )
-                    )
-                } else {
-                    _state.postValue(
-                        _state.value?.copy(
-                            isEmpty = false,
-                            isLoading = false,
-                            error = ErrorType.DATA_FETCH_ERROR,
-                        )
-                    )
-                }
-            } catch (e: Exception) {
-                Log.e("FavoritesViewModel", "Ошибка при загрузке избранных рецептов", e)
-                _state.postValue(
-                    _state.value?.copy(
-                        isEmpty = false,
-                        isLoading = false,
-                        error = ErrorType.UNKNOWN_ERROR,
-                    )
-                )
-            }*/
         }
-    }
-
-    private fun getFavorites(): Set<Int> {
-        val sharedPreferences =
-            getApplication<Application>().getSharedPreferences(
-                Constants.SHARED_PREFS_NAME,
-                Context.MODE_PRIVATE
-            )
-        val favoriteIdsStringSet =
-            sharedPreferences.getStringSet(Constants.FAVORITES_KEY, emptySet()) ?: emptySet()
-
-        return favoriteIdsStringSet.mapNotNull { it.toIntOrNull() }.toSet()
     }
 
     fun clearError() {
